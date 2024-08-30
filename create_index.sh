4880476c431e592bee46408f9eec31f49687a28f
CScott Brown <gitpushoriginmaster@gmail.com>
2024-08-30 10:31:06 -0500

    better index

--- create_index.sh
+++ create_index.sh
@@ -1,2 +1,2 @@
 rm index.html;
-find . -maxdepth 2 -type d -not -path '*/.*' | while read filename; do echo "<a href=\"$filename\">$filename</a>" >> index.html ; done;
+find . -maxdepth 2 -type d -not -path '*/.*' | while read filename; do echo "<a href=\"$filename\">$filename</a><br>" >> index.html ; done;
